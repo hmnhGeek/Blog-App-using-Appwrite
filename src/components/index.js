9274fbfdd491713fe5884b3489060156e7f52fce
hmnhGeek <hmnhsharma97@gmail.com>
2024-05-17 19:07:10 +0530

    Basic component additions

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutButton from "./Header/subcomponents/LogoutButton";
+import Input from "./Input";
 
-export { Header, Footer, Container, Logo, LogoutButton };
+export { Header, Footer, Container, Logo, LogoutButton, Input };
